1d7db63ea1757bd81cb6ea920de9dbaa2be0ecc6
qingkong88 <qingkong88@aliyun.com>
2015-04-10 03:48:56 +0800

    release
    release

--- GameObject.js
+++ GameObject.js
@@ -1,7 +1,6 @@
 (function() {
 	var wordlibs = "";
-	var defaultArr = ['E','O','I','A','U'];
-	//init words libs.
+	var defaultArr = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'D', 'L', 'C', 'U', 'M', 'W', 'F', 'G', 'Y', 'P', 'B', 'V', 'K', 'J', 'X', 'Q', 'Z'];
 
 	//GameWord
 	function GameWord(obj) {
@@ -10,6 +9,7 @@
 		this.wrongGuessCountOfCurrentWord = obj.data.wrongGuessCountOfCurrentWord;
 		this.tmpWords = "";
 		this.tmpSortArr = [];
+		this.guessedChar = "";
 		return this;
 	}
 	GameWord.prototype = {
@@ -23,33 +23,40 @@
 
 	//Game
 	function Game() {
-		this.words = [];
+		this.words = []; //GameWord list
 		this.sessionId = "";
 		this.numberOfWordsToGuess = 0;
 		this.numberOfGuessAllowedForEachWord = 0;
 		this.currentWord;
+		this.gameRunningFlag = false;
 		return this;
 	}
 	Game.prototype = {
 		//create game, get sessionId,
 		initGame: function() {
+			if(!wordlibs) return;
 			var _this = this;
+			_this.gameRunningFlag = true;
 			var startGameParam = {
 				"playerId": playerID,
 				"action": "startGame"
 			};
 			doPost(startGameParam, function(json) {
 				_this.sessionId = json.sessionId;
-				_this.numberOfWordsToGuess = json.data.numberOfWordsToGuess
+				_this.numberOfWordsToGuess = json.data.numberOfWordsToGuess;
 				_this.numberOfGuessAllowedForEachWord = json.data.numberOfGuessAllowedForEachWord;
+				//console.log("game init done..... sessionId = " + _this.sessionId);
+				_this.getWord();
 			});
 			return this;
 		},
 		//get next word 
 		getWord: function() {
 			var _this = this;
+			//console.log("get a word..... sessionId = " + _this.sessionId);
 			//all words here, game over and get result.
 			if (_this.words.length == _this.numberOfWordsToGuess) {
+				//console.log("words size is maxium..... sessionId = " + _this.sessionId);
 				_this.getResult();
 				return;
 			}
@@ -58,63 +65,106 @@
 				"sessionId": _this.sessionId,
 				"action": "nextWord"
 			};
-			this.doPost(getWordParam, function(json) {
+			doPost(getWordParam, function(json) {
 				var word = new GameWord(json);
 				_this.words.push(word);
 				_this.currentWord = word;
+				_this.currentWord.tmpSortArr = defaultArr.concat();
+				//console.log("get word..... sessionId = " + _this.sessionId + ", word index = " + _this.currentWord.totalWordCount);
+				var ch = _this.currentWord.tmpSortArr.shift();
+				//console.log("guess charator = " + ch);
+				_this.currentWord.guessedChar += ch;
+				_this.makeGuess(ch);
 			});
 			return this;
 		},
 		//guess the word
 		makeGuess: function(c) {
 			var _this = this;
+
+			if (!c) {
+				_this.getWord();
+				return;
+			}
+
 			var guessWordParam = {
 				"sessionId": _this.sessionId,
 				"action": "guessWord",
-				"guess": c
+				"guess": c.toUpperCase()
 			};
-			this.doPost(guessWordParam, function(json) {
-				//when word is correct or guess wrong count is maxium, get next word
-				if (json.data.word.indexOf("*") < 0 || json.data.wrongGuessCountOfCurrentWord == _this.numberOfGuessAllowedForEachWord) {
+			doPost(guessWordParam, function(json) {
+				//when word is correct, go to next word
+				if (json.data.word.indexOf("*") < 0) {
+					var result = "word correct. sessionId = " + _this.sessionId + ', index = ' +json.data.totalWordCount +', word = ' + json.data.word + "<br />";
+					$("#results").append(result);
 					_this.getWord();
 					return;
 				}
-				//if the word is the same, it meaning, this guess is incorrect, go next
-				if(json.data.word == _this.currentWord.currentWord){
-					_this.makeGuess(_this.currentWord.tmpSortArr.shift());
+				//when wrong counts is reach max number, go to next word
+				if (json.data.wrongGuessCountOfCurrentWord == _this.numberOfGuessAllowedForEachWord) {
+					//console.log("word incorrect attemp times goes to max, go next..... sessionId = " + _this.sessionId + ', word = ' + json.data.word);
+					_this.getWord();
 					return;
 				}
-				
+
+				//if the word is the same, it meaning, this guess is incorrect, go next
+				if (json.data.word == _this.currentWord.currentWord) {
+					//console.log("guess incorrect, guess next charator..... sessionId = " + _this.sessionId);
+					var ch = _this.currentWord.tmpSortArr.shift();
+					//console.log("guess charator = " + ch);
+					_this.currentWord.guessedChar += ch;
+					_this.makeGuess(ch);
+					return;
+				}
+
 				_this.currentWord.setValue(json);
-				_this.currentWord.tmpWords = retrieveWords(json.data.word);
-				_this.currentWord.tmpSortArr = calculationWords(_this.currentWord.tmpWords);
-				
+				_this.currentWord.tmpWords = retrieveWords(_this.currentWord.tmpWords, json.data.word);
+				_this.currentWord.tmpSortArr = calculationWords(_this.currentWord.tmpWords, _this.currentWord.guessedChar);
+
+				//fix null issue
+				if (!_this.currentWord.tmpSortArr) {
+					_this.currentWord.tmpSortArr = defaultArr.concat();
+					var ch = "";
+					do {
+						ch = _this.currentWord.tmpSortArr.shift();
+					} while (_this.currentWord.guessedChar.indexOf(ch) >= 0);
+					//console.log("guess charator = " + ch);
+					_this.currentWord.guessedChar += ch;
+					_this.makeGuess(ch);
+				} else {
+					var ch = _this.currentWord.tmpSortArr.shift();
+					//console.log("guess charator = " + ch);
+					_this.currentWord.guessedChar += ch;
+					_this.makeGuess(ch);
+				}
 			});
 			return this;
 		},
 		//get result
 		getResult: function() {
+			var _this = this;
 			var getResultParam = {
-				"sessionId": this.sessionId,
+				"sessionId": _this.sessionId,
 				"action": "getResult"
 			};
-			this.doPost(getResultParam, function(json) {
+			doPost(getResultParam, function(json) {
 				//show Result
-				$("#r_totalWordCount").html(jsonResult.data.totalWordCount);
-				$("#r_correctWordCount").html(jsonResult.data.correctWordCount);
-				$("#r_totalWrongGuessCount").html(jsonResult.data.totalWrongGuessCount);
-				$("#r_score").html(jsonResult.data.score);
+				$("#r_totalWordCount").html(json.data.totalWordCount);
+				$("#r_correctWordCount").html(json.data.correctWordCount);
+				$("#r_totalWrongGuessCount").html(json.data.totalWrongGuessCount);
+				$("#r_score").html(json.data.score);
 			});
 			return this;
 		},
 		//submit current session's result
 		submitGame: function() {
+			var _this = this;
 			var submitResult = {
-				"sessionId": this.sessionId,
+				"sessionId": _this.sessionId,
 				"action": "submitResult"
 			};
-			this.doPost(getResultParam, function(json){
-				
+			doPost(submitResult, function(json) {
+				alert("score = " + json.data.score);
 			});
 			return this;
 		}
@@ -123,29 +173,53 @@
 
 	//retrieve words from libs
 	function retrieveWords(tmpWords, word) {
-		var newReg = word.replace(/\*/g, "\[A\-Z\]");
-		var tmpArr = [];
-		if (!tmpWords) {
-			tmpArr = wordlibs.match(new RegExp(newReg, "gi"));
-		} else {
-			tmpArr = tmpWords.match(new RegExp(newReg, "gi"));
+		try {
+			//console.log("word = " + word);
+			var newReg = word.replace(/\*/g, "\[A\-Z\]");
+			var tmpArr = [];
+			if (!tmpWords) {
+				tmpArr = wordlibs.match(new RegExp(newReg, "gi"));
+			} else {
+				tmpArr = tmpWords.match(new RegExp(newReg, "gi"));
+			}
+			//fix null array, as no word in lib
+			if (!tmpArr || tmpArr.length == 0) {
+				tmpWords = "";
+			} else {
+				tmpWords = tmpArr.join(",");
+			}
+			return tmpWords;
+		} catch (e) {
+			//console.error("tmpWords = " + tmpWords);
+			//console.error("word = " + word);
+			//console.error(e);
+			return "";
 		}
-		tmpWords = tempArr.join(",");
-		return tmpWords;
 
 	};
 
-	//calculate a sort of words
-	function calculationWords(tmpWords) {
-		var tmpSortArr = tempWords.split('').sort().join('').match(/([A-Za-z])\1+/g).sort(function(b, a) {
-			return a.length - b.length;
-		});
-		var rArr = [];
-		for (var i = 0; i < tmpSortArr.length; i++) {
-			if (!tmpSortArr[i]) continue;
-			rArr.push(tmpSortArr[i][0]);
-		};
-		return rArr;
+	//calculate a sort array of words
+	function calculationWords(tmpWords, guessedChar) {
+		//fix null issue
+		if (!tmpWords) return [];
+		try {
+			var reg = new RegExp("([^," + guessedChar + "])\\1*", "gi");
+			//console.log("tmpWords.length = " + tmpWords.length);
+			var tmpSortArr = tmpWords.split('').sort().join('').match(reg).sort(function(b, a) {
+				return a.length - b.length;
+			});
+			var rArr = [];
+			for (var i = 0; i < tmpSortArr.length; i++) {
+				if (!tmpSortArr[i]) continue;
+				rArr.push(tmpSortArr[i][0]);
+			};
+			return rArr;
+		} catch (e) {
+			//console.error("tmpWords = " + tmpWords);
+			//console.error("guessedChar = " + guessedChar);
+			//console.error(e);
+			return [];
+		}
 	};
 
 
@@ -153,24 +227,34 @@
 	var playerID = "329142963@qq.com";
 	//core post data method
 	function doPost(param, callback) {
-		//		$.ajax({
-		//			type: "POST",
-		//			url: requestURL,
-		//			contentType: "application/json",
-		//			dataType: "json",
-		//			async: false,
-		//			data: JSON.stringify(param),
-		//			success: function(jsonResult) {
-		//				callback(jsonResult, data);
-		//			}
-		//		});
-		callback("test");
+		$.ajax({
+			type: "POST",
+			url: requestURL,
+			contentType: "application/json",
+			dataType: "json",
+			async: false,
+			data: JSON.stringify(param),
+			success: callback
+		});
 	};
 
-	//regiest to global
+	//init words libs.
+	function initWordlibs() {
+		var n = document.getElementById("wordsframe").contentWindow.document.all[0];
+		if (!n) {
+			setTimeout(initWordlibs, 500);
+			return;
+		} else {
+			wordlibs = n.innerText;
+			//console.log("words lib init done.....");
+		}
+	};
+	initWordlibs();
+
 	function $Game() {
 		return new Game();
-	};
+	}
 	window.$Game = $Game;
+
 
 })();
